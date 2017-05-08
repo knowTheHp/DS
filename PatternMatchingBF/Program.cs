23b5bf88d9ced8ae69a00bd158f100100ae0be73
Harpreet Singh <harpreetsingh14388@live.com>
2017-05-08 14:24:36 +0530

    Comments added

--- PatternMatchingBF/Program.cs
+++ PatternMatchingBF/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 namespace PatternMatchingBF {
+    /// <summary>
+    /// BruteForce pattern matching algorithm
+    /// </summary>
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Enter text");
@@ -9,7 +12,12 @@ namespace PatternMatchingBF {
             int matchPosition = PatternMatch(text, pattern);
             Console.WriteLine(matchPosition);
         }
-
+        /// <summary>
+        /// Bruteforce algorithm to search for a pattern in a given string.
+        /// </summary>
+        /// <param name="text">The string in which we will search the pattern.</param>
+        /// <param name="pattern">The pattern we want to search for in the string</param>
+        /// <returns>start position of the matching pattern</returns>
         static int PatternMatch(string text, string pattern) {
             for (int i = 0; i <= (text.Length - pattern.Length); i++) {
                 int j = 0;
